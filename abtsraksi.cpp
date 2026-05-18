1d384a85838c28b70de94be0ce69cd329b33dd16
hudangrh <hudangrhkuliah@gmail.com>
2026-05-18 14:37:26 +0700

    void setXY

--- abtsraksi.cpp
+++ abtsraksi.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 class Abstraksiklas {
 private: string x, y;
+
+public:
+    // method untuk mengisi nilai x dan y
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
 }
