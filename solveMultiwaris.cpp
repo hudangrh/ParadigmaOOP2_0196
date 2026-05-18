dd1bb58e5fe19c45e3c1d2605ffe2647e80960ff
hudangrh <hudangrhkuliah@gmail.com>
2026-05-18 14:49:17 +0700

    class orang public

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 class orang {
-    
-}
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+        {
+            cout << " orang di buat dengan umur " << umur << "\n" << endl;
+        }
+};
