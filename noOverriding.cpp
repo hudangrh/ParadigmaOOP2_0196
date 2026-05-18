fd0d35a987c541f371eb9a8ea6ef5da05809d286
hudangrh <hudangrhkuliah@gmail.com>
2026-05-18 15:03:30 +0700

    class derivvedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -7,3 +7,9 @@ public:
         cout << "Hallo saya Function dari baseClass";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derivedClass";
+    }
+};
